39007fd4cf3a3fdc4d10955d1b632f9486e21976
Seventeen studio <ratsimandaimanana.thierry@gmail.com>
2025-11-20 17:15:12 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.638513495e404dba9775bf4cdb4c6e5f',
+  appId: 'com.swift.donation',
   appName: 'swift-donation-app',
   webDir: 'dist',
   server: {
